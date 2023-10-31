0bf0395af87376a54922fd9e45093caf4993b4db
shivanidamerla91779963 <shivanidamerla91779963@gmail.com>
2023-10-31 17:43:13 +0530

    hellospring

---
+++ src/main/java/com/examplee/HelloWorldSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.examplee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+	}
+
+}
